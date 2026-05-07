fbb97c6074626f740c42134c7c6879d449305b16
Maisivheng <hengyagami@gmail.com>
2026-05-07 15:21:48 +0700

    add

---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import register from '@/views/register.vue'
+import login from '@/views/login.vue'
+import profile from '@/views/Profile/profile.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: 'Home',
+      },
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About',
+      },
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register,
+      meta: {
+        title: 'Register',
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login,
+      meta: {
+        title: 'Login',
+      },
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: profile,
+      meta: {
+        title: 'Profile',
+      },
+    },
+  ],
+})
+
+export default router
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Ecommerce'
+  next()
+})
