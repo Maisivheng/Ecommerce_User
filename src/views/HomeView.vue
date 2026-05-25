7f0ae1c58669a903ae8fb452117f4d12c077ea5c
Reth Sreyroth <roth64165@gmail.com>
2026-05-25 21:57:12 +0700

    dont pull

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <Navbar></Navbar>
     <section class="hero-banner-full border-bottom">
         <div class="container-fluid py-3">
-            <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between">
+            <!-- <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between">
                 <div class="col-12 col-lg-6 order-1 hero-text-content">
                     <h1 class="fw-bold mb-3" style="font-size: 36px; color: var(--primary-blue);">សម្រាប់ជីវិតឌីជីថលរបស់អ្នក</h1>
                     <p class="text-muted mb-4" style="font-size: 16px;">ស្វែងរក Laptop, Gaming PC និងគ្រឿងអេឡិចត្រូនិចទំនើបៗជាមួយតម្លៃពិសេសបំផុត។</p>
@@ -15,7 +15,7 @@
                 <div class="col-12 col-lg-6 order-2 hero-image-wrapper">
                     <img src="https://thumbs.dreamstime.com/b/bottles-global-soft-drink-brands-poznan-poland-oct-bottles-global-soft-drink-brands-including-products-coca-cola-company-233983437.jpg" class="img-fluid d-block rounded-2" alt="Hero Laptop">
                 </div>
-            </div>
+                        </div>
         </div>
     </section>
     <BaseCard></BaseCard>
